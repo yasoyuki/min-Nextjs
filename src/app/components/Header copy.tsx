3bb55fdf6f2a51a5a54ba97bee471bb403943d5a
yasoyuki <yuki1006yaso@gmail.com>
2024-02-18 20:41:10 +0900

    first commit

---
+++ src/app/components/Header copy.tsx
@@ -0,0 +1,9 @@
+
+export default function Footer() {
+  return (
+    <div className="text-xl">
+      <h2>Footer</h2>
+
+    </div>
+  );
+}
---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+  return (
+    <div className="text-xl">
+      <h2>Header</h2>
+
+    </div>
+  );
+}
